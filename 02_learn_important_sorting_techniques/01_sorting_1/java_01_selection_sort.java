44dce240fe9ba5e562cc985c0f70767fcfb3722c
yashmaheshwari0502 <yashmaheshwari0502@gmail.com>
2026-01-05 23:45:18 +0530

    fixed names

---
+++ 02_learn_important_sorting_techniques/01_sorting_1/java_01_selection_sort.java
@@ -0,0 +1,5 @@
+class java_01_selection_sort{
+    public static void main(String[] args) {
+        
+    }
+}
--- learn_important_sorting_techniques/sorting_1/selection_sort.java
+++
@@ -1,7 +0,0 @@
-
-public class selection_sort {
-    public static void main(String[] args) {
-        int arr[]={13,46,24,52,20,9};
-        
-    }
-}
